cd4d339a8ffe67429c7cf781f89ce08470248fda
Benjamin Paige <benjamin.paige@gmail.com>
2022-10-21 15:40:01 -0600

    Add doesSecretExist helper

--- docs/docs/services/mmdl.md
+++ docs/docs/services/mmdl.md
@@ -12,6 +12,6 @@ nav_order: 2
 
 - MMDL data will be available to this service from CMS Bigmac, in a single topic, with events keyed off the record’s transmittal ID.  This data will be sinked to an mmdl DyanmoDB table in this service.
 - The partition key should be the Transmittal ID of the MMDL record.
-- A DynamoDB Stream should be created as part of the mmdl service, which will be how the comparison services’s lambda is triggered.
+- A DynamoDB Stream should be created as part of the mmdl service, which will be how the comparison service's lambda is triggered.
 
 #### Notes
--- src/libs/secrets-manager-lib.js
+++ src/libs/secrets-manager-lib.js
@@ -1,10 +1,11 @@
 import {
   SecretsManagerClient,
   GetSecretValueCommand,
+  ListSecretsCommand,
 } from "@aws-sdk/client-secrets-manager";
 
 export const getSecretsValue = async ({ region, secretId }) => {
-  const client = new SecretsManagerClient({ region: region });
+  const client = new SecretsManagerClient({ region });
   const input = { SecretId: secretId };
   const command = new GetSecretValueCommand(input);
   try {
@@ -14,4 +15,16 @@ export const getSecretsValue = async ({ region, secretId }) => {
   } catch (e) {
     console.log("ERROR", JSON.stringify(e, null, 2));
   }
+};
+
+export const doesSecretExist = async (region, secretId) => {
+  const client = new SecretsManagerClient({ region });
+  const input = { Filters: [{ Key: "name", Values: [secretId] }] };
+  const command = new ListSecretsCommand(input);
+  console.log("COMMAND", JSON.stringify(command, null, 2));
+  const res = await client.send(command);
+  console.log("RES", JSON.stringify(res, null, 2));
+
+  // why is this false?
+  return res.SecretList.includes((secret) => secret.Name === secretId);
 };
