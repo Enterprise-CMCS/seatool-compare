ec8265271414bc671133e801c0bb7e84e6bfc193
Benjamin Paige <benjamin.paige@gmail.com>
2022-12-19 17:28:33 -0700

    Add check for invalid record id

--- src/services/connector/handlers/sinkMmdlData.js
+++ src/services/connector/handlers/sinkMmdlData.js
@@ -3,60 +3,51 @@ import * as dynamodb from "../../../libs/dynamodb-lib";
 async function myHandler(event, _context, _callback) {
   console.log("Received event:", JSON.stringify(event, null, 2));
 
-  // const recordKeyObject = JSON.parse(event.key);
   const recordValueObject = JSON.parse(event.value);
 
-  const possibleTransmittalKeys = Object.keys(recordValueObject).filter(
+  // Typically the PROGRAM_TYPE_CODE will match this _transNbr key
+  //   MAC: "mac179_transNbr",
+  //   CHP: "chp179_transNbr",
+  //   HHS: "hhs_transNbr",
+
+  // But the PROGRAM_TYPE_CODE might not match the expected transmittal number.
+  // As such we should use whichever _transNbr is available in the record
+  const possibleTransmittalNumberKeys = Object.keys(recordValueObject).filter(
     (k) =>
       k === "mac179_transNbr" || k === "chp179_transNbr" || k === "hhs_transNbr"
   );
 
-  if (possibleTransmittalKeys.length === 0) {
+  if (possibleTransmittalNumberKeys.length === 0) {
     console.log("No transmittal id available for the record", event);
     return;
   }
 
-  if (possibleTransmittalKeys.length > 1) {
-    console.log("Multiple transmittal ids available for the record", event);
-    console.log(possibleTransmittalKeys);
+  if (possibleTransmittalNumberKeys.length > 1) {
+    console.log(
+      "Error - Multiple transmittal ids available for the record",
+      event
+    );
+    console.log(possibleTransmittalNumberKeys);
     return;
   }
 
-  const transmittalNumberKey = possibleTransmittalKeys[0];
-
-  // if (recordKeyObject.mac179_transNbr)
-  // determine the type of record ex: chp | mac | hhs
-  // const mapOfProgramTypeToTransmittalNumberKey = {
-  //   MAC: "mac179_transNbr",
-  //   CHP: "chp179_transNbr",
-  //   HHS: "hhs_transNbr",
-  // };
-
-  // const typeOfRecord = recordKeyObject.PROGRAM_TYPE_CODE;
-  //
-  // All records have a PROGRAM_TYPE_CODE but if they are not MAC/CHP/HHS they are not relevant
-  // if (!mapOfProgramTypeToTransmittalNumberKey[typeOfRecord]) {
-  //   console.log(
-  //     "MMDL Record is not of relevent program type. PROGRAM_TYPE_CODE:",
-  //     typeOfRecord
-  //   );
-  //   return;
-  // }
-
-  // get the transmittal number based on the record type
+  const transmittalNumberKey = possibleTransmittalNumberKeys[0];
 
   try {
     const id = recordValueObject[transmittalNumberKey].FIELD_VALUE;
-    // const id =
-    //   recordValueObject[mapOfProgramTypeToTransmittalNumberKey[typeOfRecord]]
-    //     .FIELD_VALUE;
+    if (!id) {
+      console.log(
+        "Error - No valid transmittal ID value for this record",
+        event
+      );
+    }
 
     await dynamodb.update({
       tableName: process.env.tableName,
       item: { id, ...recordValueObject },
     });
   } catch (error) {
-    console.log("Error updading mmdl table", JSON.stringify(event, null, 2));
+    console.log("Error updading mmdl table", event);
     console.error(error);
   }
 }
