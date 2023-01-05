ef32c85af8461ce676b728fef0ea4255c6cdea5d
Benjamin Paige <benjamin.paige@gmail.com>
2023-01-05 14:55:20 -0700

    Update compare func

--- src/services/compare/handlers/compare.js
+++ src/services/compare/handlers/compare.js
@@ -2,13 +2,12 @@ import { getItem } from "../../../libs/dynamodb-lib";
 
 exports.handler = async function (event, context, callback) {
   console.log("Received event:", JSON.stringify(event, null, 2));
-  const result = { ...event.Payload, match: false };
-  const id = event.Context.Execution.Input.id;
-  console.log("id from context:", id);
+  const data = { ...event.Payload, match: false };
 
   try {
-    const mmdlItem = await getItem(process.env.mmdlTableName, id);
-    const seatoolItem = await getItem(process.env.seatoolTableName, id);
+    const mmdlItem = await getItem(process.env.mmdlTableName, data.id);
+    const seatoolItem = await getItem(process.env.seatoolTableName, data.id);
+
     console.log("Item from MMDL:  " + mmdlItem);
     console.log("Item from SEA Tool:  " + seatoolItem);
 
@@ -18,10 +17,45 @@ exports.handler = async function (event, context, callback) {
     if (!seatoolItem) {
       console.log("No seatool item found");
     }
+
+    // get the signature date from mmdl - this is provided in DD/MM/YYYY
+    if (
+      mmdlItem &&
+      mmdlItem.stMedDirSgnDt &&
+      mmdlItem.stMedDirSgnDt.FIELD_VALUE
+    ) {
+      data.mmdlSignedDate = mmdlItem.stMedDirSgnDt.FIELD_VALUE;
+    }
+
+    if (
+      seatoolItem &&
+      seatoolItem.STATE_PLAN &&
+      seatoolItem.STATE_PLAN.SUBMISSION_DATE
+    ) {
+      const rawDate = seatoolItem.STATE_PLAN.SUBMISSION_DATE;
+      const date = new Date(rawDate);
+      const day = date.getDate();
+      const month = date.getMonth();
+      const year = date.getFullYear();
+      const fullDate = day + "/" + (month + 1) + "/" + year;
+      data.seatoolSignedDate = fullDate;
+      console.log(fullDate);
+    }
+
+    if (
+      data.mmdlSignedDate &&
+      data.seatoolSignedDate &&
+      data.mmdlSignedDate === data.seatoolSignedDate
+    ) {
+      console.log("DATES MATCH!!");
+      data.match = true;
+    }
+
+    // get the submitted date from seatool - this is epic time x 1000 - turn it into a readable date to compare with mmdl date
   } catch (error) {
-    console.log(error);
+    console.log("Eror comparing records:", error);
   } finally {
-    console.log("Results after compare task:", JSON.stringify(result, null, 2));
-    callback(null, result);
+    console.log("Data after compare task:", JSON.stringify(data, null, 2));
+    callback(null, data);
   }
 };
--- src/services/compare/handlers/seatoolRecordExist.js
+++ src/services/compare/handlers/seatoolRecordExist.js
@@ -3,21 +3,21 @@ import { getItem } from "../../../libs/dynamodb-lib";
 exports.handler = async function (event, context, callback) {
   console.log("Received event:", JSON.stringify(event, null, 2));
   const id = event.Context.Execution.Input.id;
-  const result = { seatoolExist: false, id };
+  const data = { seatoolExist: false, id };
   try {
     const item = await getItem(process.env.seatoolTableName, id);
 
     if (item) {
-      result.seatoolExist = true;
-      result.seatoolRecord = item;
+      data.seatoolExist = true;
+      data.seatoolRecord = item;
     }
   } catch (error) {
     console.log(error);
   } finally {
     console.log(
-      `Result after finding seatool item: ${JSON.stringify(result, null, 2)}`
+      `data after finding seatool item: ${JSON.stringify(data, null, 2)}`
     );
 
-    callback(null, result);
+    callback(null, data);
   }
 };
