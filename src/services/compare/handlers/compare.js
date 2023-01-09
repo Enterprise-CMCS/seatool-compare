cdf422a37c7758ce6e4a295a40c38352a8119422
Benjamin Paige <benjamin.paige@gmail.com>
2023-01-09 14:29:05 -0700

    Remove redundancy

--- src/services/compare/handlers/compare.js
+++ src/services/compare/handlers/compare.js
@@ -1,26 +1,8 @@
-import { getItem } from "../../../libs/dynamodb-lib";
-
 exports.handler = async function (event, context, callback) {
   console.log("Received event:", JSON.stringify(event, null, 2));
   const data = { ...event.Payload, match: false };
 
   try {
-    const mmdlRecord = await getItem(process.env.mmdlTableName, data.id);
-
-    console.log("Record from MMDL:  " + mmdlRecord);
-
-    if (!mmdlRecord) {
-      console.log("No mmdl item found");
-    }
-    data.mmdlRecord = mmdlRecord;
-
-    if (
-      data.mmdlRecord.stMedDirSgnDt &&
-      data.mmdlRecord.stMedDirSgnDt.FIELD_VALUE
-    ) {
-      data.mmdlSigDate = data.mmdlRecord.stMedDirSgnDt.FIELD_VALUE;
-    }
-
     if (
       data.seatoolRecord &&
       data.seatoolRecord.STATE_PLAN &&
@@ -34,7 +16,6 @@ exports.handler = async function (event, context, callback) {
         day: "2-digit",
       });
       data.seatoolSigDate = fullDate;
-      console.log(fullDate);
     }
 
     if (
@@ -42,7 +23,6 @@ exports.handler = async function (event, context, callback) {
       data.seatoolSigDate &&
       data.mmdlSigDate === data.seatoolSigDate
     ) {
-      console.log("DATES MATCH!!");
       data.match = true;
     }
   } catch (error) {
