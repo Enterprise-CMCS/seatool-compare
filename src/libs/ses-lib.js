fe7eb19df407183416ed3bc1dd8ec6868aa34ab5
Benjamin Paige <benjamin.paige@gmail.com>
2023-01-24 15:07:58 -0700

    Update sendAttachment helper

--- src/libs/ses-lib.js
+++ src/libs/ses-lib.js
@@ -1,19 +1,21 @@
-import * as aws from "@aws-sdk/client-ses";
-import { defaultProvider } from "@aws-sdk/credential-provider-node";
+import {
+  SendRawEmailCommand,
+  SES,
+  SESClient,
+  SendEmailCommand,
+} from "@aws-sdk/client-ses";
 import nodemailer from "nodemailer";
 
-const client = new aws.SESClient({ region: process.env.region });
+const client = new SESClient({ region: process.env.region });
 
-const ses = new aws.SES({
-  apiVersion: "2010-12-01",
+const ses = new SES({
   region: process.env.region,
-  defaultProvider,
 });
 
 export async function sendAlert(params) {
   console.log("Sending email with params:", JSON.stringify(params, null, 2));
   try {
-    const command = new aws.SendEmailCommand(params);
+    const command = new SendEmailCommand(params);
     const result = await client.send(command);
     console.log("Result from sending alert:", JSON.stringify(result, null, 2));
     return result;
@@ -27,25 +29,23 @@ export async function sendAttachment() {
     from: "bpaige@gswell.com",
     subject: "This is an email sent from a Lambda function!",
     html: `<p>You got a contact message from: <b>ben</b></p>`,
-    to: "bpaige@gswell.com",
-    // bcc: Any BCC address you want here in an array,
-    // attachments: [
-    //   {
-    //     filename: "An Attachment.txt",
-    //     content: "hello world",
-    //   },
-    // ],
+    to: "bpaige@fearless.tech",
+    attachments: [
+      {
+        filename: "An Attachment.csv",
+        content: "Hello World",
+      },
+    ],
   };
 
-  console.log("Creating SES transporter");
-  // create Nodemailer SES transporter
   const transporter = nodemailer.createTransport({
-    SES: { ses, aws },
+    SES: {
+      ses: ses,
+      aws: { SendRawEmailCommand },
+    },
   });
 
   // send email
   const info = await transporter.sendMail(mailOptions);
-  console.log("Result", JSON.stringify(info, null, 2));
-
   return info;
 }
