af921f576c2156edac3b5147a8ff090ead361358
Benjamin Paige <benjamin.paige@gmail.com>
2023-01-30 09:18:32 -0700

    feat(mmdl-report): Add MMDL vs SEA Tool status reporting functionality (#74)
    * Init reporting appliance
    
    * Update sendAttachment helper
    
    * Add scan table helper
    
    * Format report data and send as csv
    
    * Update mail options
    
    * Fix typo
    
    * Add docs
    
    * typo
    
    ---------
    
    Co-authored-by: Jon Holman <9025884+JonHolman@users.noreply.github.com>

--- src/libs/ses-lib.js
+++ src/libs/ses-lib.js
@@ -1,6 +1,16 @@
-import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+import {
+  SendRawEmailCommand,
+  SES,
+  SESClient,
+  SendEmailCommand,
+} from "@aws-sdk/client-ses";
+import nodemailer from "nodemailer";
 
 const client = new SESClient({ region: process.env.region });
+
+const ses = new SES({
+  region: process.env.region,
+});
 
 export async function sendAlert(params) {
   console.log("Sending email with params:", JSON.stringify(params, null, 2));
@@ -11,5 +21,21 @@ export async function sendAlert(params) {
     return result;
   } catch (e) {
     console.error(JSON.stringify(e, null, 2));
+  }
+}
+
+export async function sendAttachment(mailOptions) {
+  const transporter = nodemailer.createTransport({
+    SES: {
+      ses: ses,
+      aws: { SendRawEmailCommand },
+    },
+  });
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    return info;
+  } catch (e) {
+    console.error("Error sending mail:", JSON.stringify(info, null, 2));
   }
 }
---
+++ src/services/compare/handlers/sendReport.js
@@ -0,0 +1,60 @@
+import {
+  sendAttachment,
+  trackError,
+  scanTable,
+  getCsvFromJson,
+} from "../../../libs";
+
+function formatReportData(data) {
+  return data.map((i) => {
+    return {
+      "Transmittal ID": i.id,
+      "Iterations ": i.iterations,
+      "Program Type": i.programType,
+      "Clock Start Date": i.mmdlSigDate,
+      "Seatool Record Exist": i.seatoolExist,
+      "Seatool Signed Date": i.seatoolSigDate || "N/A",
+      "Records Match": i.match || false,
+    };
+  });
+}
+
+function getMailOptionsWithAttachment(recipientEmail, attachment) {
+  const todaysDate = new Date().toISOString().split("T")[0];
+  const mailOptions = {
+    from: "noreply@cms.hhs.gov",
+    subject: `MMDL SEA Tool Status - ${todaysDate}`,
+    html:
+      `<p>Attached is a csv indicating the current status of MMDL and SEA Tool records.</p>` +
+      `<p>This report can be opened in your favorite spreadsheet viewing application.</p>`,
+    to: recipientEmail,
+    attachments: [
+      {
+        filename: `MMDL SEA Tool Status - ${todaysDate}.csv`,
+        content: attachment,
+      },
+    ],
+  };
+  return mailOptions;
+}
+
+exports.handler = async function (event, context, callback) {
+  console.log("Received event:", JSON.stringify(event, null, 2));
+
+  const recipientEmail = event.recipient;
+
+  if (!recipientEmail) {
+    throw 'You must manually provide a recipient email in the event to send a report. ex. {"recipient": "user@example.com"}';
+  }
+
+  try {
+    const data = await scanTable(process.env.statusTable);
+    const reportDataJson = formatReportData(data);
+    const csv = getCsvFromJson(reportDataJson);
+    const mailOptions = getMailOptionsWithAttachment(recipientEmail, csv);
+
+    await sendAttachment(mailOptions);
+  } catch (e) {
+    await trackError(e);
+  }
+};
