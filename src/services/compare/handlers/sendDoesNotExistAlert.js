458cab59eb12853114c1118b196498b6e32d9fc1
Adewale Sangobiyi <asangobiyi@ip-10-232-47-57.ec2.internal>
2023-02-01 16:59:18 -0500

    added email logic

--- src/services/compare/handlers/sendDoesNotExistAlert.js
+++ src/services/compare/handlers/sendDoesNotExistAlert.js
@@ -76,7 +76,7 @@ exports.handler = async function (event, context, callback) {
         message: `Alert for ${data.id} - sent to ${JSON.stringify(
           recipients
         )} recipient:${recipientType}`,
-      }); 
+      });
     }
   } catch (e) {
     await trackError(e);
@@ -117,6 +117,6 @@ const processEvents = (logs, id) => {
       if (event.message.includes(id)) {
           extensions[event.message.split('recipient:')[1]] = true;
       }
-  })
+  });
   return extensions;
 };
