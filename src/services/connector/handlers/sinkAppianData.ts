f988d9c839643212946874a69a18acc8e16379f8
Gavin St. Ours <gst-ours@fearless.tech>
2023-02-06 16:13:42 -0500

    Try accessing PCKG_ID for ID

---
+++ src/services/connector/handlers/sinkAppianData.ts
@@ -0,0 +1,21 @@
+import * as dynamodb from "../../../libs/dynamodb-lib";
+
+async function myHandler(
+  event: { key: string; value: string },
+  _context: any,
+  _callback: Function
+) {
+  console.log("Received event:", JSON.stringify(event, null, 2));
+  if (!process.env.tableName) {
+    throw "process.env.tableName needs to be defined.";
+  }
+
+  const eventValue = JSON.parse(event.value);
+
+  await dynamodb.putItem({
+    tableName: process.env.tableName,
+    item: { id: eventValue.payload.PCKG_ID, ...JSON.parse(event.value) },
+  });
+}
+
+exports.handler = myHandler;
