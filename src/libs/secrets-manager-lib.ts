a84dd8da4c075b8866a1312e263197fc5846d188
Adewale Sangobiyi <asangobiyi@ip-10-232-43-39.ec2.internal>
2023-02-15 13:58:04 -0500

    feat(initial release): undate logic for email notification

--- src/libs/secrets-manager-lib.ts
+++ src/libs/secrets-manager-lib.ts
@@ -5,6 +5,7 @@ import {
 } from "@aws-sdk/client-secrets-manager";
 
 export const getSecretsValue = async (region: string, secretId: string) => {
+  console.log("region", region, "secretId", secretId);
   const client = new SecretsManagerClient({ region });
   const input = { SecretId: secretId };
   const command = new GetSecretValueCommand(input);
@@ -13,6 +14,7 @@ export const getSecretsValue = async (region: string, secretId: string) => {
     const result = JSON.parse(response.SecretString ?? "");
     return result;
   } catch (e) {
+    console.log("getSecretsValue ERROR - ", e);
     console.log("getSecretsValue ERROR", JSON.stringify(e, null, 2));
   }
 };
