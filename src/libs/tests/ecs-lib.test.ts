f02318fc2be3b368a6ad53031af037eb3b81f494
James Dinh <jdinh@collabralink.com>
2023-03-10 08:43:26 -0800

    rename tests

---
+++ src/libs/tests/ecs-lib.test.ts
@@ -0,0 +1,31 @@
+import { getSecretsValue, doesSecretExist } from "../secrets-manager-lib";
+import { it, describe, expect } from "vitest";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+  ListSecretsCommand,
+} from "@aws-sdk/client-secrets-manager";
+import { mockClient } from "aws-sdk-client-mock";
+
+const secretClientMock = mockClient(SecretsManagerClient);
+
+describe("secrets manager lib", () => {
+  it("should successfully return secret value", async () => {
+    const secret = { Name: "Secret", SecretString: JSON.stringify("test") };
+    secretClientMock.on(GetSecretValueCommand).resolves(secret);
+
+    const response = await getSecretsValue("test-region", "email");
+
+    expect(response).toEqual("test");
+  });
+
+  it("should successfully return if secret exist", async () => {
+    secretClientMock
+      .on(ListSecretsCommand)
+      .resolves({ SecretList: [{ Name: "email" }] });
+
+    const response = await doesSecretExist("test-region", "email");
+
+    expect(response).toEqual(true);
+  });
+});
