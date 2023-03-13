04e608c186934c031e329222d21b8550b77e2f65
Benjamin Paige <benjamin.paige@gmail.com>
2023-03-13 14:23:17 -0600

    Update email template

--- src/services/compare-mmdl/handlers/utils/getEmailContent.ts
+++ src/services/compare-mmdl/handlers/utils/getEmailContent.ts
@@ -21,10 +21,11 @@ export const getEmailContent = ({
                 } reminder that there's no matching record in <a href="https://sea.cms.gov/" style="text-decoration:none" target="_blank">SEA Tool</a> for ${id}.</h2>
                 <br>
                 <p>Either a record wasn't created in SEA Tool, or the SPA ID in MMDL and SEA Tool don't match.</p>
-                ${programType ?? `<p>Program Type: ${programType}.</p>`}
+                ${programType ? `<p>Program Type: ${programType}.</p>` : ""}
                 ${
-                  isUrgent ??
-                  "<em>Failure to address this could lead to critical delays in the review process and a deemed approved SPA or waiver action.</em>"
+                  isUrgent
+                    ? "<em>Failure to address this could lead to critical delays in the review process and a deemed approved SPA or waiver action.</em>"
+                    : ""
                 }
                 <br>
                 <div style=' background-color: rgb(22, 82, 150); width: 580px; padding: 20px; margin: 20px; color: white;'> if you have any questions, please contact the help desk at <a href="mailto:SEATool_HelpDesk@cms.hhs.gov" style="color:#ffffff;">SEATool_HelpDesk@cms.hhs.gov</a> </div>
