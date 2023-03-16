e8aec9990a038cc60c059465a6c9ae0a6037453d
Benjamin Paige <benjamin.paige@gmail.com>
2023-03-16 11:37:32 -0600

    fix(alert content): updating alert content (#122)
    * Turn off wfs
    
    * Turn on workflows for cooler branch
    
    * Add seatoolLink param to alert
    
    * Add seatoolLink param to sendAlert func
    
    * Add back id for email
    
    * Add dot
    
    * Remove width from email footer
    
    * Add delete perm to connector service
    
    * Update email content
    
    * Update subdomain func
    
    * Update env param

--- src/services/compare-mmdl/handlers/utils/getEmailContent.ts
+++ src/services/compare-mmdl/handlers/utils/getEmailContent.ts
@@ -1,11 +1,13 @@
 export const getEmailContent = ({
   id,
   isUrgent,
-  programType,
+  seatoolSubdomain = "sea",
+  isCHP,
 }: {
   id: string;
   isUrgent: boolean;
-  programType?: string;
+  seatoolSubdomain?: string;
+  isCHP: boolean;
 }) => {
   const htmlData = `
         <html lang='en'>
@@ -18,18 +20,18 @@ export const getEmailContent = ({
                 <center>
                 <h2>This is ${
                   isUrgent ? "an urgent" : "a"
-                } reminder that there's no matching record in <a href="https://sea.cms.gov/" style="text-decoration:none" target="_blank">SEA Tool</a> for ${id}${
-    programType ? ` - Program Type: ${programType}` : ""
-  }.</h2>
+                } reminder that there's no matching record in <a href="https://${seatoolSubdomain}.cms.gov/" style="text-decoration:none" target="_blank">SEA Tool</a> for ${id}.</h2>
                 <br>
                 <p>Either a record wasn't created in SEA Tool, or the SPA ID in MMDL and SEA Tool don't match.</p>
                 ${
                   isUrgent
-                    ? "<em>Failure to address this could lead to critical delays in the review process and a deemed approved SPA or waiver action.</em>"
+                    ? `<em>Failure to address this could lead to critical delays in the review process and a deemed approved SPA$${
+                        isCHP ? `.` : ` or waiver action.`
+                      }</em>`
                     : ""
                 }
                 <br>
-                <div style=' background-color: rgb(22, 82, 150); width: 580px; padding: 20px; margin: 20px; color: white;'> if you have any questions, please contact the help desk at <a href="mailto:SEATool_HelpDesk@cms.hhs.gov" style="color:#ffffff;">SEATool_HelpDesk@cms.hhs.gov</a> </div>
+                <div style=' background-color: rgb(22, 82, 150); padding: 20px; margin: 20px; color: white;'>If you have any questions, please contact the help desk at <a href="mailto:SEATool_HelpDesk@cms.hhs.gov" style="color:#ffffff;">SEATool_HelpDesk@cms.hhs.gov</a> </div>
                 </center>
             </body>
         </html>
