9ce8b9e07c5430d49bbecd696a9c13f22ca9949b
Gavin St. Ours <gst-ours@fearless.tech>
2023-04-05 16:35:13 -0400

    Further expand tests

--- src/services/compare-appian/handlers/tests/sendReport.test.ts
+++ src/services/compare-appian/handlers/tests/sendReport.test.ts
@@ -1,13 +1,28 @@
-import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import {
+  MockedFunction,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
 import * as sendReport from "../sendReport";
 import * as libs from "../../../../libs";
 
+const handler = sendReport as { handler: Function };
+
 vi.mock("../../../../libs", () => {
   return {
-    scanTable: vi.fn().mockImplementation(() => appianReportData),
+    getCsvFromJson: vi.fn(),
+    scanTable: vi.fn(),
+    sendAttachment: vi.fn(),
     trackError: vi.fn(),
   };
 });
+
+const submissionDateString = "1680723474372";
 
 const appianReportData = [
   {
@@ -16,58 +31,110 @@ const appianReportData = [
     isAppianSubmitted: true,
     SPA_ID: "test-id",
     iterations: 0,
-    secSinceAppianSubmitted: 123,
-    appianSubmittedDate: 1234567890,
+    secSinceAppianSubmitted: "12345678",
+    appianSubmittedDate: submissionDateString,
     seatoolExist: true,
-    seatoolSubmissionDate: 1234567890, // optional
-    match: true, // optional
+    seatoolSubmissionDate: submissionDateString,
+    match: true,
   },
 ];
 
-const handler = sendReport as { handler: Function };
+const event = { recipient: "recipient@example.com" };
 
 describe("sendReport", () => {
-  afterEach(() => {
-    vi.restoreAllMocks();
-  });
-
-  it("should log the received event in the expected format", async () => {
-    process.env.statusTable = "test-table";
+  beforeEach(() => {
     vi.spyOn(console, "log");
-    const event = { recipient: "recipient@example.com" };
-    await handler.handler(event);
+    vi.spyOn(libs, "trackError");
+  });
 
-    expect(console.log).toHaveBeenCalledWith(
-      "Received event:",
-      JSON.stringify(event, null, 2)
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("throws an error if process.env.statusTable is not defined", async () => {
+    await expect(handler.handler(event)).rejects.toThrowError(
+      "process.env.statusTable needs to be defined."
     );
   });
 
-  it("throws an error if no email provided", async () => {
-    const event = { recipient: null };
-    await expect(() =>
-      handler
-        .handler(event)
-        .rejects.toThrowError(
-          'You must manually provide a recipient email in the event to send a report. ex. {"recipient": "user@example.com"}'
-        )
-    );
-  });
+  describe("when process.env.statusTable is defined", () => {
+    beforeAll(() => {
+      process.env.statusTable = "test-table";
+    });
 
-  it("throws an error if process.env.statusTable is not set", async () => {
-    await expect(() =>
-      handler
-        .handler(event)
-        .rejects.toThrowError("process.env.statusTable needs to be defined.")
-    );
-  });
+    it("logs the received event in the expected format", async () => {
+      vi.spyOn(console, "log");
+      await handler.handler(event);
 
-  it("gets data from scanTable", async () => {
-    const event = { recipient: "recipient@example.com" };
-    process.env.statusTable = "test-table";
+      expect(console.log).toHaveBeenCalledWith(
+        "Received event:",
+        JSON.stringify(event, null, 2)
+      );
+    });
 
-    await handler.handler(event);
-    expect(libs.scanTable).toHaveBeenCalledOnce();
-    expect(libs.trackError).not.toHaveBeenCalled();
+    it("throws an error if no email provided", async () => {
+      const event = { recipient: null };
+      await expect(() =>
+        handler
+          .handler(event)
+          .rejects.toThrowError(
+            'You must manually provide a recipient email in the event to send a report. ex. {"recipient": "user@example.com"}'
+          )
+      );
+    });
+
+    it("calls scanTable as expected", async () => {
+      await handler.handler(event);
+      expect(libs.scanTable).toBeCalledWith({
+        TableName: "test-table",
+      });
+    });
+
+    it("does not attempt to format data if undefined", async () => {
+      await handler.handler(event);
+      expect(libs.getCsvFromJson).not.toBeCalled();
+      expect(libs.sendAttachment).not.toBeCalled();
+      expect(libs.trackError).not.toBeCalled();
+    });
+
+    it("formats data and sends attachment", async () => {
+      (
+        libs.scanTable as MockedFunction<typeof libs.scanTable>
+      ).mockResolvedValue(appianReportData);
+
+      await handler.handler(event);
+
+      expect(libs.getCsvFromJson).toBeCalledWith([
+        {
+          PCKG_ID: "test-pk",
+          "SPA ID": "test-id",
+          "Iterations ": 0,
+          "Submission Date": formatDate(Number(submissionDateString)),
+          "Seatool Record Exist": true,
+          "Seatool Signed Date": formatDate(Number(submissionDateString)),
+          "Records Match": true,
+        },
+      ]);
+
+      const todaysDate = new Date().toISOString().split("T")[0];
+      expect(libs.sendAttachment).toBeCalledWith({
+        attachments: [
+          {
+            content: undefined,
+            filename: `Appian SEA Tool Status - ${todaysDate}.csv`,
+          },
+        ],
+        from: "noreply@cms.hhs.gov",
+        html: "<p>Attached is a csv indicating the current status of Appian and SEA Tool records.</p><p>This report can be opened in your favorite spreadsheet viewing application.</p>",
+        subject: `Appian SEA Tool Status - ${todaysDate}`,
+        to: "recipient@example.com",
+      });
+    });
   });
 });
+
+function formatDate(dateMs: number) {
+  return new Date(dateMs).toLocaleString("en-US", {
+    timeZone: "America/New_York",
+  });
+}
