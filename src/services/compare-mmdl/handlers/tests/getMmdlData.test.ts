15ee78e0d4af0cb003073f4bdf47007e534ab11d
Gavin St. Ours <gst-ours@fearless.tech>
2023-04-10 15:45:08 -0400

    feat(mmdl unit tests): Add unit tests for MMDL handler functions (#143)
    * Improve error handling
    
    * Add initial test
    
    * Allow lodash to work with vitest
    
    * Add console error testing
    
    * Expand initial pass on getAppianData tests
    
    * Improve test descriptions
    
    * Improve getAppianData tests
    
    * initStatus tests
    
    * seatoolRecordExist tests
    
    * sendNoMatchAlert tests
    
    * sendReport tests
    
    * stopExecutions tests
    
    * Update key format
    
    * Expand stopExecutions tests
    
    * Fix test statement
    
    * Expand updateStatus tests
    
    * Remove skip
    
    * Expand worlflowStarter tests
    
    * Minor cleanup
    
    * Expand testing for the “no match” alert
    
    * Temporarily disable failing test
    
    * Further expand email tests
    
    * Fix date formatting bug
    
    * Further expand tests
    
    * Add initial tests for getMmdlData handler
    
    * Fix bug: string caused func to return undefined
    
    * Expand getMmdlData tests
    
    * Add getStartAtTimeStamp unit tests
    
    * Fix typo
    
    * Add seatoolRecordExist unit tests
    
    * Add sendNoMatchAlert tests
    
    * Remove stray console log statement
    
    * Add sendReport tests
    
    * Add stopExecution tests
    
    * Add workflowStarter tests

---
+++ src/services/compare-mmdl/handlers/tests/getMmdlData.test.ts
@@ -0,0 +1,114 @@
+import {
+  MockedFunction,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
+import * as getMmdlData from "../getMmdlData";
+import * as libs from "../../../../libs";
+
+const handler = getMmdlData as { handler: Function };
+const callback = vi.fn();
+
+const testPK = "test-pk";
+const testSK = "test-sk";
+
+const event = {
+  Payload: {
+    PK: testPK,
+    SK: testSK,
+  },
+};
+
+const mmdlReportData = {
+  PK: testPK,
+  SK: testSK,
+  mmdlSigDate: "1680785774788",
+  clockStartDate: "1680785774788",
+};
+
+vi.mock("../../../../libs", async () => {
+  return {
+    getItem: vi.fn().mockImplementation(() => mmdlReportData),
+    trackError: vi.fn(),
+  };
+});
+
+describe("getMmdlData", () => {
+  beforeEach(() => {
+    vi.spyOn(console, "log");
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("throws an error if process.env.mmdlTableName is not defined", async () => {
+    await expect(() =>
+      handler.handler(event, null, callback)
+    ).rejects.toThrowError("process.env.mmdlTableName needs to be defined.");
+  });
+
+  describe("with process.env.mmdlTableName defined", () => {
+    beforeAll(() => {
+      process.env.mmdlTableName = "test-table";
+    });
+
+    it("does not call trackError with properly-formatted data", async () => {
+      await handler.handler(event, null, callback);
+      expect(libs.trackError).not.toBeCalled();
+    });
+
+    it("logs the received event", async () => {
+      await handler.handler(event, null, callback);
+      expect(console.log).toBeCalledWith(
+        "Received event:",
+        JSON.stringify(event, null, 2)
+      );
+    });
+
+    it("calls getItem with the expected parameters", async () => {
+      await handler.handler(event, null, callback);
+      expect(libs.getItem).toBeCalledWith({
+        tableName: "test-table",
+        key: { PK: testPK, SK: testSK },
+      });
+    });
+
+    it("passes data to the callback as expected", async () => {
+      await handler.handler(event, null, callback);
+      const callbackData = callback.mock.calls[0][1];
+      console.log(callbackData);
+      expect(callbackData).toHaveProperty("programType");
+      expect(callbackData).toHaveProperty("TN");
+    });
+
+    it("calculates secSinceMmdlSigned as a number", async () => {
+      await handler.handler(event, null, callback);
+      const callbackData = callback.mock.calls[0][1];
+      expect(callbackData).toHaveProperty("secSinceMmdlSigned");
+      expect(callbackData["secSinceMmdlSigned"]).toBeTypeOf("number");
+    });
+
+    it("calculates secSinceClockStart as a number", async () => {
+      await handler.handler(event, null, callback);
+      const callbackData = callback.mock.calls[0][1];
+      expect(callbackData).toHaveProperty("secSinceClockStart");
+      expect(callbackData["secSinceClockStart"]).toBeTypeOf("number");
+    });
+
+    it("logs the returning data", async () => {
+      await handler.handler(event, null, callback);
+      const callbackData = callback.mock.calls[0][1];
+
+      expect(console.log).toBeCalledWith(
+        `Returning data `,
+        JSON.stringify(callbackData, null, 2)
+      );
+    });
+  });
+});
