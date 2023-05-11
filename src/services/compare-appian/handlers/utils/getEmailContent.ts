309fbec8fdf436c842eaba898b3b48a322505923
James Dinh <jdinh@collabralink.com>
2023-05-11 11:44:04 -0700

    add missing email logic

--- src/services/compare-appian/handlers/utils/getEmailContent.ts
+++ src/services/compare-appian/handlers/utils/getEmailContent.ts
@@ -1,9 +1,11 @@
 export const getEmailContent = ({
   id,
   isUrgent,
+  seatoolSubdomain = "sea",
 }: {
   id: string;
   isUrgent: boolean;
+  seatoolSubdomain?: string;
 }) => {
   const htmlData = `
     <html lang='en'>
@@ -16,16 +18,16 @@ export const getEmailContent = ({
         <center>
         <h2>This is ${
           isUrgent ? "an urgent" : "a"
-        } reminder that there's no matching record in SEA Tool for ${id}.</h2>
+        } reminder that there's no matching record in <a href="https://${seatoolSubdomain}.cms.gov/" style="text-decoration:none" target="_blank">SEA Tool</a> for ${id}.</h2>
         <br>
         <p>Either a record wasn't created in SEA Tool, or the SPA ID in Appian and SEA Tool don't match.</p>
         ${
           isUrgent
-            ? "<em>Failure to address this could lead to critical delay in the review process and a deemed aproved SPA.</em>"
+            ? "<em>Failure to address this could lead to critical delays in the review process and a deemed approved SPA action.</em>"
             : ""
         }
         <br>
-        <div style=' background-color: rgb(22, 82, 150); width: 580px; padding: 20px; margin: 20px; color: white;'> if you have any questions, please contact the help desk at <a href="mailto:SEATool_helpDesk@cms.hhs.org" style="color:#ffffff;">SEATool_helpDesk@cms.hhs.org</a> </div>
+        <div style=' background-color: rgb(22, 82, 150); padding: 20px; margin: 20px; color: white;'>Please respond to this email address, <a href="mailto:SEATool_HelpDesk@cms.hhs.gov" style="color:#ffffff;">SEATool_HelpDesk@cms.hhs.gov</a>, if you have any further questions. </div>
         </center>
       </body>
     </html>
