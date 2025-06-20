c5f7efbc68ac82fff11469c024338b716cb3c124
Benjamin Paige <benjamin.paige@gmail.com>
2025-06-20 14:58:49 -0600

    Update for code ql

--- src/services/dashboard/handlers/templatizeCloudWatchDashboard.ts
+++ src/services/dashboard/handlers/templatizeCloudWatchDashboard.ts
@@ -1,4 +1,5 @@
 import { CloudWatch } from "@aws-sdk/client-cloudwatch";
+import _ from "lodash";
 import type {
   APIGatewayEvent,
   APIGatewayProxyCallback,
@@ -11,17 +12,18 @@ export const handler = async (
   _callback: APIGatewayProxyCallback
 ) => {
   //environment variables passed to lambda from serverless.yml
-  const service = process.env.service!;
-  const accountId = process.env.accountId!;
-  const stage = process.env.stage!;
-  const region = process.env.region!;
+  const service = _.escapeRegExp(process.env.service!);
+  const accountId = _.escapeRegExp(process.env.accountId!);
+  const stage = _.escapeRegExp(process.env.stage!);
+  const region = _.escapeRegExp(process.env.region!);
 
   const client = new CloudWatch({});
   const dashboard = await client.getDashboard({
     DashboardName: `${service}-${stage}`,
   });
   const templateJson = dashboard
-    .DashboardBody!.replace(new RegExp(accountId, 'g'), "${aws:accountId}")
+    .DashboardBody!
+    .replace(new RegExp(accountId, 'g'), "${aws:accountId}")
     .replace(new RegExp(stage, 'g'), "${sls:stage}")
     .replace(new RegExp(region, 'g'), "${env:REGION_A}")
     .replace(new RegExp(service, 'g'), "${self:service}");
